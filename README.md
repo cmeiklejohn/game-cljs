9aef283702a22eda5a39958bed43cb1da5679072
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-03-19 10:43:10 -0700

    Initial.

---
+++ README.md
@@ -0,0 +1,13 @@
+# game
+
+The Game, authored in ClojureScript.
+
+## Usage
+
+You think Cheese don't know this here game?
+
+## License
+
+Copyright © 2013 Christopher Meiklejohn
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,30 @@
+;; Copyright (c) Christopher Meiklejohn. All rights reserved.
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 which can be found in the file
+;; LICENSE.html at the root of this distribution.  By using this
+;; software in any fashion, you are agreeing to be bound by the terms of
+;; this license. You must not remove this notice, or any other, from
+;; this software.
+;;
+(defproject game "0.0.1-SNAPSHOT"
+  :description "The Game, authored in ClojureScript"
+  :url "https://github.com/cmeiklejohn/game-cljs"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :plugins [[lein-cljsbuild "0.3.0"]
+            [net.drib/strokes "0.4.0"]]
+  :source-paths ["src/clj" "src/cljs"]
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild
+    {:builds
+     [{:source-paths ["src/cljs"],
+       :id "core",
+       :compiler
+       {:pretty-print true,
+        :output-to "resources/private/core.js",
+        :optimizations :whitespace}}
+      ]})
